5aaaf358c16d5e5a487242ec718ba20ec4c01a62
Dambaev Alexander <ice.redmine@gmail.com>
2022-03-17 15:35:25 +0800

    fix : add missing 'config' option

--- local-build-container.nix
+++ local-build-container.nix
@@ -1,16 +1,18 @@
 {pkgs, lib, ...}:
 {
   containers.build = {
-    imports = [
-      ./host.nix
-    ];
     privateNetwork = true;
     hostAddress = "192.168.100.16";
     localAddress = "192.168.100.17";
-    networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
-    nixpkgs.config.allowUnfree = true;
-    environment.systemPackages = with pkgs; [
-      wget vim
-    ];
+    config = {
+      imports = [
+        ./host.nix
+      ];
+      networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+      nixpkgs.config.allowUnfree = true;
+      environment.systemPackages = with pkgs; [
+        wget vim
+      ];
+    };
   };
 }
