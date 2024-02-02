9889a69ff3a9e0b50e848deed1c80c290b0022d3
Dambaev Alexander <ice.redmine@gmail.com>
2024-02-02 20:51:16 +0800

    refactor: use one repo for dev and for production instances

--- host.nix
+++ host.nix
@@ -3,17 +3,10 @@ env@{
 , OP_ENERGY_REPO_LOCATION ? /etc/nixos/.git/modules/overlays/op-energy/modules/oe-blockspan-service
 , OP_ENERGY_ACCOUNT_REPO_LOCATION ? /etc/nixos/.git/modules/overlays/op-energy
   # import psk from out-of-git file
-  # TODO: switch to secrets-manager and change to make it more secure
-, bitcoind-signet-rpc-psk ? builtins.readFile ( "/etc/nixos/private/bitcoind-signet-rpc-psk.txt")
-  # TODO: refactor to autogenerate HMAC from the password above
-, bitcoind-signet-rpc-pskhmac ? builtins.readFile ( "/etc/nixos/private/bitcoind-signet-rpc-pskhmac.txt")
-, op-energy-db-psk-signet ? builtins.readFile ( "/etc/nixos/private/op-energy-db-psk-signet.txt")
-, op-energy-db-salt-signet ? builtins.readFile ( "/etc/nixos/private/op-energy-db-salt-signet.txt")
 , bitcoind-mainnet-rpc-psk ? builtins.readFile ( "/etc/nixos/private/bitcoind-mainnet-rpc-psk.txt")
 , op-energy-db-psk-mainnet ? builtins.readFile ( "/etc/nixos/private/op-energy-db-psk-mainnet.txt")
 , op-energy-db-salt-mainnet ? builtins.readFile ( "/etc/nixos/private/op-energy-db-salt-mainnet.txt")
 , op-energy-account-token-encryption-key ? builtins.readFile ( "/etc/nixos/private/op-energy-account-token-encryption-key.txt")
-, mainnet_node_ssh_tunnel ? true # by default we want ssh tunnel to main node, but this is useless for github actions as they are using only signet node
 }:
 args@{ pkgs, lib, config, ...}:
 
@@ -48,72 +41,9 @@ in
     opEnergyAccountServiceModule
   ];
   system.stateVersion = "22.05";
+
   # op-energy part
   services.op-energy-backend = {
-  # keeping testnet commented to have testnet ports in quick access
-  #  testnet = {
-  #    db_user = "topenergy";
-  #    db_name = "topenergy";
-  #    db_psk = op-energy-db-psk-testnet;
-  #    config = ''
-  #      {
-  #        "MEMPOOL": {
-  #          "NETWORK": "testnet",
-  #          "BACKEND": "none",
-  #          "HTTP_PORT": 8997,
-  #          "API_URL_PREFIX": "/api/v1/",
-  #          "POLL_RATE_MS": 2000
-  #        },
-  #        "CORE_RPC": {
-  #          "USERNAME": "top-energy",
-  #          "PASSWORD": "${bitcoind-testnet-rpc-psk}",
-  #          "PORT": 18332
-  #        },
-  #        "DATABASE": {
-  #          "ENABLED": true,
-  #          "HOST": "127.0.0.1",
-  #          "PORT": 3306,
-  #          "DATABASE": "topenergy",
-  #          "ACCOUNT_DATABASE": "topenergyacc",
-  #          "USERNAME": "topenergy",
-  #          "PASSWORD": "${op-energy-db-psk-testnet}"
-  #        },
-  #        "STATISTICS": {
-  #          "ENABLED": true,
-  #          "TX_PER_SECOND_SAMPLE_PERIOD": 150
-  #        }
-  #      }
-  #    '';
-  #  };
-    signet =
-      let
-        db = "sopenergy";
-      in {
-      db_user = "sopenergy";
-      db_name = db;
-      db_psk = op-energy-db-psk-signet;
-      account_db_name = "${db}acc";
-      config = ''
-        {
-          "DB_PORT": 5432,
-          "DB_HOST": "127.0.0.1",
-          "DB_USER": "${db}",
-          "DB_NAME": "${db}",
-          "DB_PASSWORD": "${op-energy-db-psk-signet}",
-          "SECRET_SALT": "${op-energy-db-salt-signet}",
-          "API_HTTP_PORT": 8995,
-          "BTC_URL": "http://127.0.0.1:38332",
-          "BTC_USER": "sop-energy",
-          "BTC_PASSWORD": "${bitcoind-signet-rpc-psk}",
-          "BTC_POLL_RATE_SECS": 10,
-          "PROMETHEUS_PORT": 7995,
-          "SCHEDULER_POLL_RATE_SECS": 10
-        }
-      '';
-    };
-  } // (if !mainnet_node_ssh_tunnel
-    then {}
-    else {
     mainnet =
       let
         db = "openergy";
@@ -140,12 +70,8 @@ in
         }
       '';
     };
-  });
-  # enable op-energy-frontend service
-  services.op-energy-frontend = {
-    enable = true;
-    signet_enabled = true;
   };
+
   services.op-energy-account-service = {
     enable = true;
     db_name = "openergyacc";
@@ -168,31 +94,9 @@ in
     '';
   };
 
-  # bitcoind signet instance
-  services.bitcoind.signet = {
+  # enable op-energy-frontend service
+  services.op-energy-frontend = {
     enable = true;
-    dataDir = "/mnt/bitcoind-signet";
-    extraConfig = ''
-      txindex = 1
-      server=1
-      listen=1
-      discover=1
-      rpcallowip=127.0.0.1
-      # those option affects memory footprint of the instance, so changing the default value
-      # will affect the ability to shrink the node's resources.
-      # default value is 450 MiB
-      # dbcache=3700
-      # default value is 125, affects RAM occupation
-      # maxconnections=1337
-      signet = 1
-      [signet]
-    '';
-    rpc.users = {
-      sop-energy = {
-        name = "sop-energy";
-        passwordHMAC = "${bitcoind-signet-rpc-pskhmac}";
-      };
-    };
   };
 
   # List packages installed in system profile. To search, run:
@@ -204,6 +108,7 @@ in
     iftop # network usage monitor
     git
   ];
+
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
@@ -213,34 +118,10 @@ in
     80
     443 # ssl backed service
   ];
-  systemd.services = if !mainnet_node_ssh_tunnel
-    then {}
-    else {
-    ssh_tunnel = { # we use ssh tunnel to production instance in order to reuse connection to mainnet node. This service's goal is just to keep ssh tunnel alive all the time
-      wantedBy = [ "multi-user.target" ];
-      before = [ "op-energy-backend-mainnet.service" ];
-      after = [
-        "network-online.target"
-      ];
-      requires = [
-        "network-online.target"
-      ];
-      serviceConfig = {
-        Type = "simple";
-        Restart = "always"; # we want to keep service always running
-        StartLimitIntervalSec = 0;
-        StartLimitBurst = 0;
-      };
-      path = with pkgs; [
-        openssh
-      ];
-      script = ''
-        ssh proxy@exchange.op-energy.info -L8332:127.0.0.1:8332 -oServerAliveInterval=60 -n "while true; do sleep 10s; done"
-      '';
-    };
-  };
+
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
   networking.firewall.logRefusedConnections = false; # we are not interested in a logs of refused connections
+
 }
--- local_settings_op-energy-dev.nix
+++ local_settings_op-energy-dev.nix
@@ -6,7 +6,7 @@ args@{ pkgs, lib, ...}:
     acceptTerms = true;
     defaults.email = "ice.redmine+oe+acme@gmail.com";
     certs = {
-      "dev-exchange.op-energy.info" = {
+      "dev-exchange.op.energy" = {
         webroot = "/var/lib/acme/acme-challenge/";
         email = "ice.redmine+oe+acme@gmail.com";
         # Ensure that the web server you use can read the generated certs
@@ -15,16 +15,67 @@ args@{ pkgs, lib, ...}:
         # Since we have a wildcard vhost to handle port 80,
         # we can generate certs for anything!
         # Just make sure your DNS resolves them.
-        extraDomainNames = [ "dev-exchange.op.energy" ];
+        extraDomainNames = [ "dev-exchange.op-energy.info" ];
       };
     };
   };
+
   services.nginx = {
     virtualHosts = {
       op-energy = {
         forceSSL = true;
-        useACMEHost = "dev-exchange.op-energy.info";
+        useACMEHost = "dev-exchange.op.energy";
       };
     };
   };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    screen
+    atop # process monitor
+    tcpdump # traffic sniffer
+    iftop # network usage monitor
+    git
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [
+    22
+    80
+    443 # ssl backed service
+  ];
+
+  systemd.services = {
+    ssh_tunnel = { # we use ssh tunnel to production instance in order to reuse connection to mainnet node. This service's goal is just to keep ssh tunnel alive all the time
+      wantedBy = [ "multi-user.target" ];
+      before = [ "op-energy-backend-mainnet.service" ];
+      after = [
+        "network-online.target"
+      ];
+      requires = [
+        "network-online.target"
+      ];
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always"; # we want to keep service always running
+        StartLimitIntervalSec = 0;
+        StartLimitBurst = 0;
+      };
+      path = with pkgs; [
+        openssh
+      ];
+      script = ''
+        ssh proxy@exchange.op-energy.info -L8332:127.0.0.1:8332 -oServerAliveInterval=60 -n "while true; do sleep 10s; done"
+      '';
+    };
+  };
+
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+  networking.firewall.logRefusedConnections = false; # we are not interested in a logs of refused connections
 }
