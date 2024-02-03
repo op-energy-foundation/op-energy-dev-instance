eb2603c72434ad960c2b2647b13f49357ed51740
Dambaev Alexander <ice.redmine@gmail.com>
2024-02-04 01:59:50 +0800

    ditto

--- local_settings_op-energy-dev.nix
+++ local_settings_op-energy-dev.nix
@@ -1,6 +1,10 @@
 args@{ pkgs, lib, ...}:
 
 {
+  imports = [
+    ./local_settings_development.nix # this instance is development
+  ];
+
   users.users.nginx.extraGroups = [ "acme" ];
   security.acme = {
     acceptTerms = true;
@@ -29,53 +33,8 @@ args@{ pkgs, lib, ...}:
     };
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    screen
-    atop # process monitor
-    tcpdump # traffic sniffer
-    iftop # network usage monitor
-    git
-  ];
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
-    22
-    80
     443 # ssl backed service
   ];
-
-  systemd.services = {
-    ssh_tunnel = { # we use ssh tunnel to production instance in order to reuse connection to mainnet node. This service's goal is just to keep ssh tunnel alive all the time
-      wantedBy = [ "multi-user.target" ];
-      before = [ "op-energy-backend-mainnet.service" ];
-      after = [
-        "network-online.target"
-      ];
-      requires = [
-        "network-online.target"
-      ];
-      serviceConfig = {
-        Type = "simple";
-        Restart = "always"; # we want to keep service always running
-        StartLimitIntervalSec = 0;
-        StartLimitBurst = 0;
-      };
-      path = with pkgs; [
-        openssh
-      ];
-      script = ''
-        ssh proxy@exchange.op-energy.info -L8332:127.0.0.1:8332 -oServerAliveInterval=60 -n "while true; do sleep 10s; done"
-      '';
-    };
-  };
-
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = true;
-  networking.firewall.logRefusedConnections = false; # we are not interested in a logs of refused connections
 }
