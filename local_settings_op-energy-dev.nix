767de3e3f7d73feec2cca6ac978081d9df49628c
Natsvora <naimishvora13@gmail.com>
2026-08-31 22:35:57 +0530

    fix: add account, blockspan, and offer service proxies to .info domain nginx

--- local_settings_op-energy-dev.nix
+++ local_settings_op-energy-dev.nix
@@ -83,6 +83,32 @@ in
           serverName = "dev-exchange.op-energy.info";
           forceSSL = true;
           useACMEHost = "dev-exchange.op.energy";
+          extraConfig = ''
+            location /api/v2/account {
+                    proxy_pass http://127.0.0.1:8899/api/v2/account;
+            }
+            location /api/v1/account {
+                    proxy_pass http://127.0.0.1:8899/api/v1/account;
+            }
+            location /api/v2/strikes/blockrate {
+                    proxy_pass http://127.0.0.1:8899/api/v2/strikes/blockrate;
+            }
+            location /api/v1/blocktime {
+                    proxy_pass http://127.0.0.1:8899/api/v1/blocktime;
+            }
+            location /api/v2/blockspans {
+                    proxy_pass http://127.0.0.1:8999/api/v2/blockspans;
+            }
+            location /api/v1/ws {
+                    proxy_pass http://127.0.0.1:8999/api/v1/ws;
+                    proxy_http_version 1.1;
+                    proxy_set_header Upgrade $http_upgrade;
+                    proxy_set_header Connection "upgrade";
+            }
+            location /api/v2/offer {
+                    proxy_pass http://127.0.0.1:8909/api/v2/offer;
+            }
+          '';
         }
         ( lib.recursiveUpdate
           (op-energy-frontend-prototype-subroute "/prototype")
