1d0c6a5fddc884ec2da9c4ff9533688699d5ffeb
Dambaev Alexander <ice.redmine@gmail.com>
2026-09-09 23:27:04 +0800

    refactor: use systemd's credentials for lnbits and litd services

--- litd_service.nix
+++ litd_service.nix
@@ -52,16 +52,21 @@ in
       requires = [ "postgresql.service" ];
       serviceConfig = {
         Type = "simple";
+        LoadCredential =
+          [ "litd_ui_password:/etc/nixos/private/litd_ui_password.pass"
+            "bitcoind-signet-rpc-psk:/etc/nixos/bitcoind-signet-rpc-psk.pass"
+          ];
       };
       path = with pkgs; [
         lightning-terminal postgresql
       ];
       preStart = '' exit 0 '';
 
+
       script = ''
        litd \
          --insecure-httplisten=127.0.0.1:${toString cfg.http_port} \
-         --uipassword=${litd_ui_password} \
+         --uipassword=$(cat \${CREDENTIALS_DIRECTORY}/litd_ui_password) \
          --network=signet \
          --lnd-mode=integrated \
          --lnd.lnddir=/root/.lnd \
@@ -72,7 +77,7 @@ in
          --lnd.bitcoin.node=bitcoind \
          --lnd.bitcoind.rpchost=localhost \
          --lnd.bitcoind.rpcuser=sop-energy \
-         --lnd.bitcoind.rpcpass=${bitcoind-signet-rpc-psk} \
+         --lnd.bitcoind.rpcpass=$(cat \${${CREDENTIALS_DIRECTORY}/bitcoind-signet-rpc-psk}) \
          --lnd.bitcoind.zmqpubrawblock=localhost:28332 \
          --lnd.bitcoind.zmqpubrawtx=localhost:28333 \
          --lnd.debuglevel=debug \
@@ -81,7 +86,7 @@ in
          --faraday.connect_bitcoin \
          --faraday.bitcoin.host=localhost \
          --faraday.bitcoin.user=sop-energy \
-         --faraday.bitcoin.password=${bitcoind-signet-rpc-psk}
+         --faraday.bitcoin.password=$(cat \${${CREDENTIALS_DIRECTORY}/bitcoind-signet-rpc-psk})
       '';
     };
   };
