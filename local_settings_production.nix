73e1c5a6357c6f94f5e2265e4022196292213689
Dambaev Alexander <ice.redmine@gmail.com>
2026-09-09 14:45:24 +0800

    feat: local_settings_production.nix: enable signet bitcoind instance for a future use of lightning network

--- local_settings_production.nix
+++ local_settings_production.nix
@@ -1,6 +1,8 @@
 env@{
   bitcoind-mainnet-rpc-psk ? builtins.readFile ( "/etc/nixos/private/bitcoind-mainnet-rpc-psk.txt")
 , bitcoind-mainnet-rpc-pskhmac ? builtins.readFile ( "/etc/nixos/private/bitcoind-mainnet-rpc-pskhmac.txt")
+, bitcoind-signet-rpc-psk ? builtins.readFile ( "/etc/nixos/private/bitcoind-signet-rpc-psk.txt")
+, bitcoind-signet-rpc-pskhmac ? builtins.readFile ( "/etc/nixos/private/bitcoind-signet-rpc-pskhmac.txt")
 , op-energy-db-psk-mainnet ? builtins.readFile ( "/etc/nixos/private/op-energy-db-psk-mainnet.txt")
 , op-energy-db-salt-mainnet ? builtins.readFile ( "/etc/nixos/private/op-energy-db-salt-mainnet.txt")
 , ...
@@ -17,21 +19,41 @@ in
   system.stateVersion = "22.05";
 
   # bitcoind mainnet instance
-  services.bitcoind.mainnet = {
-    enable = true;
-    dataDir = "/mnt/bitcoind-mainnet";
-    extraConfig = ''
-      txindex = 1
-      server=1
-      listen=1
-      discover=1
-      rpcallowip=0.0.0.0/0
-      rpcbind=0.0.0.0
-    '';
-    rpc.users = {
-      op-energy = {
-        name = "op-energy";
-        passwordHMAC = "${bitcoind-mainnet-rpc-pskhmac}";
+  services.bitcoind = {
+    mainnet = {
+      enable = true;
+      dataDir = "/mnt/bitcoind-mainnet";
+      extraConfig = ''
+        txindex = 1
+        server=1
+        listen=1
+        discover=1
+        rpcallowip=127.0.0.1/32
+        rpcbind=127.0.0.1
+      '';
+      rpc.users = {
+        op-energy = {
+          name = "op-energy";
+          passwordHMAC = "${bitcoind-mainnet-rpc-pskhmac}";
+        };
+      };
+    };
+    signet = {
+      enable = true;
+      extraCmdlineOptions = [ "-signet" ];
+      extraConfig = ''
+        txindex = 1
+        server=1
+        listen=1
+        discover=1
+        rpcallowip=127.0.0.1/32
+        rpcbind=127.0.0.1
+      '';
+      rpc.users = {
+        op-energy = {
+          name = "sop-energy";
+          passwordHMAC = "${bitcoind-signet-rpc-pskhmac}";
+        };
       };
     };
   };
