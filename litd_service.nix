5404454d68ee0d1e9600d69239e52caf30ccfda2
Dambaev Alexander <ice.redmine@gmail.com>
2026-09-09 23:28:05 +0800

    refactor: use systemd's credentials for lnbits and litd services

--- litd_service.nix
+++ litd_service.nix
@@ -77,7 +77,7 @@ in
          --lnd.bitcoin.node=bitcoind \
          --lnd.bitcoind.rpchost=localhost \
          --lnd.bitcoind.rpcuser=sop-energy \
-         --lnd.bitcoind.rpcpass=$(cat \${${CREDENTIALS_DIRECTORY}/bitcoind-signet-rpc-psk}) \
+         --lnd.bitcoind.rpcpass=$(cat \${CREDENTIALS_DIRECTORY}/bitcoind-signet-rpc-psk}) \
          --lnd.bitcoind.zmqpubrawblock=localhost:28332 \
          --lnd.bitcoind.zmqpubrawtx=localhost:28333 \
          --lnd.debuglevel=debug \
@@ -86,7 +86,7 @@ in
          --faraday.connect_bitcoin \
          --faraday.bitcoin.host=localhost \
          --faraday.bitcoin.user=sop-energy \
-         --faraday.bitcoin.password=$(cat \${${CREDENTIALS_DIRECTORY}/bitcoind-signet-rpc-psk})
+         --faraday.bitcoin.password=$(cat \${CREDENTIALS_DIRECTORY}/bitcoind-signet-rpc-psk})
       '';
     };
   };
